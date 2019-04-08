205da1689839d5514151fbc013f9a1b4441485a8
Yuriy Kwak <kvakyur@gmail.com>
2019-04-08 10:58:04 +0300

    доработка прото файлов в части package

--- IIoT_Platform/tests/testBrocaar.py
+++ IIoT_Platform/tests/testBrocaar.py
@@ -1,13 +1,13 @@
 
-from protos.ns import ns_pb2_grpc
-from protos.ns import ns_pb2
 import grpc
+from protos.aps import user_pb2
+from protos.aps import user_pb2_grpc
 
 
 def run():
     gateway_request = ns_pb2.Gateway()
     gateway_request.id = bytes(bytearray.fromhex('7276ff000b031819'))
-    with grpc.insecure_channel('192.168.10.28:8000') as channel:
+    with grpc.insecure_channel('192.168.10.28:8080') as channel:
         res = ns_pb2_grpc.NetworkServerServiceStub(channel).GetVersion(gateway_request)
         print(res)
 
